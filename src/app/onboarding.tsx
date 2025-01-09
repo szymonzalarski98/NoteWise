2a539f4c018cd7fc69e996059b8cc7f3c2dca0a1
Szymon <szymonzalarski98@gmail.com>
2025-01-09 09:12:35 +0100

    feat: minimal working setup for LLM NW-1

--- src/app/onboarding.tsx
+++ src/app/onboarding.tsx
@@ -1,54 +1,92 @@
-import { useRouter } from 'expo-router';
-import React from 'react';
+// Helpful reference: https://github.dev/software-mansion/react-native-executorch/blob/main/examples/llama/components/Messages.tsx
+import React, { useEffect, useState } from 'react';
+import { LLAMA3_2_1B_QLORA, useLLM } from 'react-native-executorch';
 
-import { Cover } from '@/components/cover';
 import {
+  ActivityIndicator,
   Button,
-  FocusAwareStatusBar,
+  Input,
   SafeAreaView,
   Text,
   View,
 } from '@/components/ui';
-import { useIsFirstTime } from '@/lib/hooks';
-export default function Onboarding() {
-  const [_, setIsFirstTime] = useIsFirstTime();
-  const router = useRouter();
-  return (
-    <View className="flex h-full items-center  justify-center">
-      <FocusAwareStatusBar />
-      <View className="w-full flex-1">
-        <Cover />
-      </View>
-      <View className="justify-end ">
-        <Text className="my-3 text-center text-5xl font-bold">
-          Obytes Starter
-        </Text>
-        <Text className="mb-2 text-center text-lg text-gray-600">
-          The right way to build your mobile app
-        </Text>
 
-        <Text className="my-1 pt-6 text-left text-lg">
-          🚀 Production-ready{' '}
-        </Text>
-        <Text className="my-1 text-left text-lg">
-          🥷 Developer experience + Productivity
-        </Text>
-        <Text className="my-1 text-left text-lg">
-          🧩 Minimal code and dependencies
-        </Text>
-        <Text className="my-1 text-left text-lg">
-          💪 well maintained third-party libraries
-        </Text>
+export default function Onboarding() {
+  const [message, setMessage] = useState('');
+
+  const {
+    generate,
+    isGenerating,
+    isReady,
+    isModelReady,
+    downloadProgress,
+    response,
+    isModelGenerating,
+  } = useLLM({
+    modelSource: LLAMA3_2_1B_QLORA,
+    tokenizerSource: require('../../assets/tokenizer.bin'),
+    contextWindowLength: 6,
+  });
+
+  const askLLM = async () => {
+    try {
+      await generate(message);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  // INFO: for debugging purposes only
+  useEffect(() => {
+    if (response && !isModelGenerating) {
+      console.log('Message: ', response, 'end');
+    }
+
+    const interval = setInterval(() => {
+      console.log('Debug - Response:', response);
+      console.log('Debug - isModelGenerating:', isModelGenerating);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [response, isModelGenerating]);
+
+  return (
+    <SafeAreaView>
+      <View className="flex h-full p-2">
+        <View>
+          <Text>Statuses:</Text>
+          <Text>Download Progress: {downloadProgress.toString()}</Text>
+          <Text>isReady: {isReady.toString()}</Text>
+          <Text>isModelReady: {isModelReady.toString()}</Text>
+          <Text>isGenerating: {isGenerating.toString()}</Text>
+        </View>
+        <View className="flex-1 bg-gray-100">
+          <Text>Response: {response}</Text>
+        </View>
+        <View className="relative">
+          {isGenerating && (
+            <View className="absolute inset-0 bg-gray-100">
+              <ActivityIndicator />
+            </View>
+          )}
+          <Input
+            placeholder="Enter your message"
+            value={message}
+            onChangeText={setMessage}
+          />
+          <Button
+            onPress={askLLM}
+            label="Ask"
+            disabled={
+              isGenerating ||
+              !isModelReady ||
+              !isReady ||
+              !message ||
+              message === ''
+            }
+          />
+        </View>
       </View>
-      <SafeAreaView className="mt-6">
-        <Button
-          label="Let's Get Started "
-          onPress={() => {
-            setIsFirstTime(false);
-            router.replace('/login');
-          }}
-        />
-      </SafeAreaView>
-    </View>
+    </SafeAreaView>
   );
 }
